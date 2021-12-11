99629216832a07d5bf501762f03ebce7e27fdbb2
plmaurel69 <88190008+plmaurel69@users.noreply.github.com>
2021-12-11 14:17:39 +0100

    completed" "

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only [:edit, :update, :show, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -19,12 +20,16 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
@@ -33,5 +38,8 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :details, :completed)
   end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
 end
